929a705411836089df3918a849b808d50150739e
Yevhen Mukhamedov <Yevhen_Mukhamedov@epam.com>
2021-02-16 10:04:00 +0200

    Task-6

---
+++ src/classes/index.ts
@@ -0,0 +1,6 @@
+export * from "./referenceItem"
+export * from "./universityLibrarian"
+export { default as RefBook } from "./encyclopedia"
+export { Reader } from "./reader"
+
+export type { Library } from "./library"
